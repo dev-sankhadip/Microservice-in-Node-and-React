8c0e5be9eb9624ec99cad4a6116a147d185602a1
Sankhadip Samanta <sankha@Sankhadips-MacBook-Air.local>
2022-09-10 08:42:35 +0530

    project initialized

---
+++ event-bus/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+const events = [];
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+  console.log(`Received event ${event.type}`);
+
+  axios.post("http://post-clusterip-srv:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://comment-clusterip-srv:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://query-clsuterip-srv:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://moderation-clusterip-srv:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
+});
+
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
